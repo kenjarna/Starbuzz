0b6ea61407d7ac31684a6e8eb12d2891f7e8e567
Kenny Jarnagin <jarnagink19@hanover.edu>
2018-05-21 10:51:49 -0400

    Initial commit

---
+++ app/src/main/java/edu/hanover/starbuzz/TopLevelActivity.java
@@ -0,0 +1,30 @@
+package edu.hanover.starbuzz;
+
+import android.content.Intent;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.view.View;
+
+public class TopLevelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener () {
+            public void onItemClick(AdapterView<?> listView,
+                                    View v, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+}
